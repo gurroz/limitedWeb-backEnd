0ad23ea480d8b9985adea34702188747349e5531
Gonzalo Urroz <gonzalo.urroz@cursor.cl>
2018-09-23 21:57:52 +1000

    Efforst to connect to GCloud

--- backend/src/main/java/au/com/rmit/limitedWeb/backend/controller/HelloController.java
+++ backend/src/main/java/au/com/rmit/limitedWeb/backend/controller/HelloController.java
@@ -2,13 +2,21 @@ package au.com.rmit.limitedWeb.backend.controller;
 
 import au.com.rmit.limitedWeb.backend.model.Projects;
 import au.com.rmit.limitedWeb.backend.model.ProjectsRepository;
+import au.com.rmit.limitedWeb.backend.services.ProjectService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 @RestController
 public class HelloController {
+
+    static final Logger LOG = LoggerFactory.getLogger(HelloController.class);
 
     @Autowired
     private ProjectsRepository projectsRepository;
@@ -19,9 +27,19 @@ public class HelloController {
     }
 
     @RequestMapping(path="/all")
-    public @ResponseBody
-    Iterable<Projects> getAllProjects() {
+    public @ResponseBody Iterable<Projects> getAllProjects() {
         // This returns a JSON or XML with the users
         return projectsRepository.findAll();
+    }
+
+    @RequestMapping(path="/create")
+    public void createProject() {
+        try {
+            ProjectService.getInstance().createProject();
+        } catch (IOException e) {
+            LOG.error("Error IOException crear proyecto", e);
+        } catch (GeneralSecurityException e) {
+            LOG.error("Error GeneralSecurityException crear proyecto", e);
+        }
     }
 }
